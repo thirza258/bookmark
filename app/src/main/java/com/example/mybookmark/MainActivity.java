8cd8a55ea0084793f7e9a5502d90a7d55bc71104
thirza258 <thirzahmad@gmail.com>
2021-09-27 15:46:06 +0700

    -commit

--- app/src/main/java/com/example/mybookmark/MainActivity.java
+++ app/src/main/java/com/example/mybookmark/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.mybookmark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linear;
+    private RecyclerView recyclerView, faq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
 
+    }
+
+    public void initView() {
+        linear = findViewById(R.id.linear);
+        recyclerView = findViewById(R.id.reclycleView);
+        faq = findViewById(R.id.faq);
     }
 }
